2776aaa7eb982aa48896e1bf446933e4978e9543
samarth1345 <76591895+samarth1345@users.noreply.github.com>
2022-05-15 06:02:34 +0530

    //

--- lib/screens/admin_folder/widgets/homepage.dart
+++ lib/screens/admin_folder/widgets/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class makecard extends StatelessWidget {
   String text;
@@ -15,45 +16,115 @@ class makecard extends StatelessWidget {
       required this.color2});
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 150,
-      height: 100,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            gradient: LinearGradient(colors: [color1, color2])),
-        // padding: EdgeInsets.all(10),
-        margin: EdgeInsets.only(top: 10),
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Row(
-                children: <Widget>[
-                  Icon(
-                    icon,
-                    size: 35,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    data,
-                    style: TextStyle(fontSize: 25, color: Colors.white),
-                  )
-                ],
-              ),
-              Text(
-                text,
-                style: TextStyle(fontSize: 15, color: Colors.white),
-              ),
-            ],
+    return InkWell(
+      child: SizedBox(
+        width: 150,
+        height: 100,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(colors: [color1, color2])),
+          // padding: EdgeInsets.all(10),
+          margin: EdgeInsets.only(top: 10),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      icon,
+                      size: 35,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      data,
+                      style: TextStyle(fontSize: 25, color: Colors.white),
+                    )
+                  ],
+                ),
+                Text(
+                  text,
+                  style: TextStyle(fontSize: 15, color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
       ),
+      onTap: () {},
     );
   }
 }
 
-class homepage extends StatelessWidget {
+Widget makeListTile(String title, IconData icon, String data, String department,
+    Color color1, Color color2) {
+  return Container(
+    decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [color1, color2, color1]),
+        borderRadius: BorderRadius.circular(10)),
+    child: ListTile(
+        tileColor: Colors.transparent,
+        textColor: Colors.white,
+        iconColor: Colors.white,
+        leading: Icon(
+          icon,
+          size: 30,
+        ),
+        title: Text(
+          title,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Row(
+          children: [
+            Icon(
+              Icons.star_border,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              data,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Text(
+              department,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+        trailing: Icon(Icons.more_vert_rounded)),
+  );
+}
+
+class homepage extends StatefulWidget {
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  Map<String, double> data = new Map();
+  List<Color> _colors = [
+    Colors.teal,
+    Colors.blueAccent,
+    Colors.amberAccent,
+    Colors.redAccent
+  ];
+  final colorList = <Color>[
+    Colors.greenAccent,
+  ];
+
+  @override
+  void initState() {
+    data.addAll(
+        {'Mess': 20, 'Room cleaning': 10, 'Electricity': 30, 'Plumber': 5});
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -101,6 +172,47 @@ class homepage extends StatelessWidget {
                     ]),
               )
             ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Divider(
+            height: 5,
+            color: Colors.black54,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          makeListTile('Best Rating', Icons.thumb_up_sharp, '4.5', 'Mess',
+              Colors.green, Colors.green.shade300),
+          SizedBox(
+            height: 5,
+          ),
+          makeListTile(
+              'Worst Rating',
+              Icons.thumb_down_sharp,
+              '2.5',
+              'Room Cleaning',
+              Color.fromARGB(255, 220, 39, 26),
+              Colors.red.shade300),
+          SizedBox(
+            height: 15,
+          ),
+          Divider(
+            height: 5,
+            color: Colors.black54,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          PieChart(
+            dataMap: data, // if not declared, random colors will be chosen
+            animationDuration: Duration(milliseconds: 1500),
+            chartLegendSpacing: 32.0,
+            chartRadius: MediaQuery.of(context).size.width / 2.0,
+            colorList: _colors, //determines the size of the chart
+            chartType: ChartType.disc, //can be changed to ChartType.ring
+            emptyColor: Colors.white60,
           )
         ]),
       ),
